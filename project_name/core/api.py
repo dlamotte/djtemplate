c0b16baeea026f8ac18cfe5c38c9cae5287bf89f
Dan LaMotte <lamotte85@gmail.com>
2012-09-23 18:09:45 -0500

    adding basic user serialization

--- project_name/core/api.py
+++ project_name/core/api.py
@@ -1,3 +1,11 @@
+from django.db.models import get_model
 from tastypie.api import Api
+from tastypie.resources import ModelResource
 
 api = Api(api_name='v1')
+
+class UserResource(ModelResource):
+    class Meta:
+        queryset = get_model('auth', 'User').objects.all()
+        exclude = ('password', )
+api.register(UserResource())
--- project_name/core/context_processors.py
+++ project_name/core/context_processors.py
@@ -1,6 +1,17 @@
 from django.conf import settings
+from {{ project_name }}.core.api import UserResource
 
 def default(request):
+    user = 'null'
+    res_user = UserResource()
+    if hasattr(request, 'user') and request.user.is_authenticated():
+        user = res_user.serialize(
+            None,
+            res_user.full_dehydrate(res_user.build_bundle(obj=request.user)),
+            'application/json'
+        )
+
     return {
+        'user_json': user,
         'settings': settings,
     }
