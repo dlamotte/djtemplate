52176b9f03e07406d01a8fdf2b72db95b5950ad3
Dan LaMotte <lamotte85@gmail.com>
2012-09-23 21:27:22 -0500

    pylibmc doesnt compile on heroku

--- project_name/settings.py
+++ project_name/settings.py
@@ -5,12 +5,6 @@ TEMPLATE_DEBUG = DEBUG = False
 COMPRESS_ENABLED = True
 
 CACHES['default'].update({
-    'BACKEND': 'django_pylibmc.memcached.PyLibMCCache',
+    'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
     'LOCATION': '127.0.0.1:11211',
-    'TIMEOUT': 500,
-    'BINARY': True,
-    'OPTIONS': {
-        'tcp_nodelay': True,
-        'ketama': True
-    }
 })
