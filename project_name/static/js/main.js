0868622e0acff8fe7a6e58f635c25afb83fc0998
Dan LaMotte <lamotte85@gmail.com>
2012-10-02 21:59:42 -0500

    main.js: adding in a useful utility function

--- project_name/static/js/main.js
+++ project_name/static/js/main.js
@@ -1,5 +1,18 @@
 (function($, window, undefined) {
+  var g = APP.g = {};
   var models = APP.models = {};
+  var utils = APP.utils = {};
   var views = APP.views = {};
-  APP.g = {};
+
+  utils.get_template = function(idsuffix) {
+    if (! g.template_cache) {
+      g.template_cache = {};
+    }
+    if (! g.template_cache[idsuffix]) {
+      template_cache[idsuffix] = _.template(
+        document.getElementById('template-' + idsuffix).innerHTML
+      );
+    }
+    return g.template_cache[idsuffix];
+  };
 }(jQuery, this));
