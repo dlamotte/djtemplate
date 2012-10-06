c77cd7b54277231c6a817654eaf3ba6b18798f28
Dan LaMotte <lamotte85@gmail.com>
2012-10-06 14:54:02 -0500

    models: connect new user to create_api_key signal

--- project_name/core/models.py
+++ project_name/core/models.py
@@ -1,1 +1,10 @@
+from django.contrib.auth.models import User
+from django.db import models
+from django.db.models import get_model, signals
+from tastypie.models import create_api_key
+
 # Create your models here.
+
+
+# signals
+signals.post_save.connect(create_api_key, sender=User)
