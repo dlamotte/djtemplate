ae7c35201b4042dd106afa25d04e5d5c077ffc18
Dan LaMotte <lamotte85@gmail.com>
2012-10-09 09:05:43 -0500

    removing bogus "as api"

--- project_name/core/urls.py
+++ project_name/core/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import patterns, include, url
-from {{ project_name }}.core.api import api as api
+from {{ project_name }}.core.api import api
 
 urlpatterns = patterns('{{ project_name }}.core.views',
     url(r'^api/', include(api.urls)),
