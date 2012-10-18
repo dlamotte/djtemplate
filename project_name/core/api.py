19ce7061e7de73ea5b8d5647c8d95eb5ad07b36d
Dan LaMotte <lamotte85@gmail.com>
2012-10-18 14:29:36 -0500

    api: more complete UserResource example

--- project_name/core/api.py
+++ project_name/core/api.py
@@ -1,11 +1,24 @@
 from django.db.models import get_model
 from tastypie.api import Api
+from tastypie.authorization import DjangoAuthorization
+from tastypie.constants import ALL, ALL_WITH_RELATIONS
 from tastypie.resources import ModelResource
+from {{ project_name }}.core.authentication import ApiKeyAuthentication
 
 api = Api(api_name='v1')
 
 class UserResource(ModelResource):
     class Meta:
         queryset = get_model('auth', 'User').objects.all()
+        always_return_data = True
+        authentication = ApiKeyAuthentication()
+        authorization = DjangoAuthorization()
         exclude = ('password', )
-api.register(UserResource())
+        filtering = {
+            'username': ALL,
+            'first_name': ALL,
+            'last_name': ALL,
+            'email': ALL,
+        }
+
+api.regisier(UserResource())
